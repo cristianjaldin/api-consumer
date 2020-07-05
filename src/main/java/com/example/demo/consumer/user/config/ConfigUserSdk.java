15bd4b2e091f4fe95db8150c98026426837670e1
Cristian Jaldin <cristianjaldin@hotmail.com>
2020-07-05 14:55:35 -0300

    changes

--- src/main/java/com/example/demo/consumer/user/config/ConfigUserSdk.java
+++ src/main/java/com/example/demo/consumer/user/config/ConfigUserSdk.java
@@ -2,7 +2,7 @@ package com.example.demo.consumer.user.config;
 
 import org.springframework.context.annotation.Configuration;
 
-import com.example.demo.sdk.user.config.EnableUserSdk;
+import com.example.demo.sdk.user.configSDK.EnableUserSdk;
 
 @Configuration
 @EnableUserSdk
--- src/main/java/com/example/demo/consumer/user/controller/UserController.java
+++ src/main/java/com/example/demo/consumer/user/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.demo.consumer.user.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.user.UserCreateDto;
+import com.example.demo.dto.user.UserDto;
 import com.example.demo.sdk.user.service.UserSdk;
 
 
@@ -15,14 +21,34 @@ public class UserController {
 	@Autowired
 	private UserSdk userSdk;
 	
-	@GetMapping("/test")
-	public ResponseEntity<String> probando() {
+	@GetMapping("/test/users")
+	public ResponseEntity<List<UserDto>> getAll() {
 		
-		System.out.println("probando");
-		userSdk.findAll();
-		System.out.println("termino prueba");
+		List<UserDto> users = userSdk.findAll();
+		users.forEach(System.out::println);
+	
+		return new ResponseEntity<List<UserDto>>(users, HttpStatus.OK);
+	}
+
+
+	@GetMapping("/test/add")
+	public ResponseEntity<UserDto> add() {
 		
-		return new ResponseEntity<String>("probando", HttpStatus.OK);
+		//usuario a ingresar
+		UserCreateDto userCreateDto =  new UserCreateDto();
+		userCreateDto.setName("Criss");
+		userCreateDto.setSurname("Jaldin");
+		userCreateDto.setUsername("cjaldin");
+		userCreateDto.setPassword("1234");
+		userCreateDto.setRoles(Arrays.asList("ADMIN"));
+
+		
+		UserDto userCreated = userSdk.add(userCreateDto);
+		
+		
+		System.out.println(userCreated);
+		
+		return new ResponseEntity<UserDto>(userCreated, HttpStatus.OK);
 	}
 
 	
